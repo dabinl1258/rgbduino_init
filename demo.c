5a8ed7087ffdc257eff6fb5e590801ffe7944253
wolp <ldbl1258@gmail.com>
2023-04-25 19:08:28 +0900

    edit

--- demo.c
+++ demo.c
@@ -18,72 +18,36 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
-
-#include "iocompat.h"		/* Note [1] */
-
-enum { UP, DOWN };
-
-ISR (TIMER1_OVF_vect)		/* Note [2] */
+void Delay(int tick);
+int Init_Pin()
 {
-    static uint16_t pwm;	/* Note [3] */
-    static uint8_t direction;
+    DDRB = 0xff;
+    DDRC = 0xff;
+    DDRD = 0xff;
+    
+    PORTB = 0x00;
+    PORTC = 0x00;
+    PORTD = 0x00;    
+    Delay(1000);
 
-    switch (direction)		/* Note [4] */
-    {
-        case UP:
-            if (++pwm == TIMER1_TOP)
-                direction = DOWN;
-            break;
-
-        case DOWN:
-            if (--pwm == 0)
-                direction = UP;
-            break;
-    }
-
-    OCR = pwm;			/* Note [5] */
-}
-
-void
-ioinit (void)			/* Note [6] */
-{
-    /* Timer 1 is 10-bit PWM (8-bit PWM on some ATtinys). */
-    TCCR1A = TIMER1_PWM_INIT;
-    /*
-     * Start timer 1.
-     *
-     * NB: TCCR1A and TCCR1B could actually be the same register, so
-     * take care to not clobber it.
-     */
-    TCCR1B |= TIMER1_CLOCKSOURCE;
-    /*
-     * Run any device-dependent timer 1 setup hook if present.
-     */
-#if defined(TIMER1_SETUP_HOOK)
-    TIMER1_SETUP_HOOK();
-#endif
-
-    /* Set PWM value to 0. */
-    OCR = 0;
-
-    /* Enable OC1 as output. */
-    DDROC = _BV (OC1);
-
-    /* Enable timer 1 overflow interrupt. */
-    TIMSK = _BV (TOIE1);
-    sei ();
-}
-
+    PORTB |= 0x01;
+    PORTC |= 0x01;
+    PORTD |= 0x04;
+}   
 int
 main (void)
 {
 
-    ioinit ();
-
+     Init_Pin();   
     /* loop forever, the interrupts are doing the rest */
-
     for (;;)			/* Note [7] */
         sleep_mode();
-
+       
+    
     return (0);
+}
+void Delay(int tick)
+{
+    for(int i = 0; i < tick; i++)
+    ;;
 }
